5b00d98b4d59596fc15a83b57da4118e582495cf
talgins47 <talgins47@gmail.com>
2024-05-21 17:23:24 +0300

    states and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
+import {useState} from 'react'
 
-function Post(){
+interface PostProps {
+  title: string
+  content: string[]
+  onItemSelected: (index: number) => void
+}
 
+function PostList({title, content}: PostProps){
+  console.log("Post")
+  const [clickedIndex, setClickedIndex] = useState(-1)
+  // const array: string[]=[]
+
+  const onClick = (index:number, item: string): void => {
+    console.log("You clicked on item", index, item)
+    setClickedIndex(index)
+  }
   return (
+    <>
+     <h1>{title}</h1>
+
+    {content.length === 0 && <p>There are no items</p>}
     <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>
+      {
+      content.map(
+        (item, index) => {
+          return (
+            <li 
+            
+              className={(clickedIndex == index)? "list-group-item active" : "list-group-item"} 
+              key = {index}
+              onClick={()=>onClick(index, item)}
+              >An item: {item} index {index}</li>
+            )
+        }
+      )}
+    </ul>
+    </>
   )
 }
 
 
 function App() {
+  console.log("App")
+    const array = ["An item", "A second item", "A third item", "A fourth item", "And a fifth item"]
+    //array for colors
+    const array2 = ["red", "blue", "green", "yellow", "purple"]
   return (
-    <>
-      <h1>My First List</h1>
-      <Post />
-    </>
+    <div>
+      <div className = "alert alert-warning alert dismissble fade show" role = "alert">
+        <strong>Warning!</strong> A simple warning alert—check it out!
+        <button type = "button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <PostList title='This My Title' content={array} onItemSelected={(index)=> {console.log("item selected in first list" + index)}} />
+      <PostList title='This My Color' content={array2} onItemSelected={(index)=> {console.log("color selected " + array2[index])}}/>
+      <button type = "button" className="btn btn-primary" onClick={()=> {console.log("click")}}>primary</button>
+
+    
+    </div>
      
   )
 }
+
 
 export default App
